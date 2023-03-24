2a4a9975fbefedbfd2182cda84e33185eec180f2
Eric Khosrafian <erickhosrafian@gmail.com>
2023-03-25 00:26:38 +0430

    added sequential search

---
+++ Search/sequentialSearch.js
@@ -0,0 +1,7 @@
+const radnomArray = [5, 3, 5, 8, 5, , 66, 6, 6, 55, 47, 62, 84];
+
+const sequentialSearch = (array, key) => {
+  for (const index of array) if (index === key) return index;
+};
+
+//
